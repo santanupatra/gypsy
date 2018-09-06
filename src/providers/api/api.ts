91b14bb9ec56473db42247f8d9cf303dc3d27372
DebashreeNatitsolved <debashree.chandra@natitsolved.com>
2018-09-06 18:51:42 +0530

    debashree_06_09

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -8,6 +8,7 @@ const apiUrl = "http://111.93.169.90/team4/gypsy/webservice";
 //const apiUrl = "http://192.168.1.118/team4/gypsy/webservice";
 const mediaUrl ="http://111.93.169.90/team4/gypsy/";
 const url ="http://111.93.169.90";
+const baseurl="http://111.93.169.90/team4/gypsy/api";
 
 
 @Injectable()
@@ -31,6 +32,15 @@ export class ApiProvider {
   	return this.http.post(apiUrl+'/'+link, data).map(response => {
 
     
+      	return response;
+    });
+
+  }
+
+  likelist(link,data){
+  console.log(data);
+	return this.http.post(baseurl+'/'+link, data).map(response => {
+   
       	return response;
     });
 
